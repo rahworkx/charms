e00f4a4c2e8ea06cf8ebe30d1b705baab91dcb9a
Ronald.Haynes <rhaynes008@gmail.com>
2017-06-07 15:25:17 -0400

    Updated some more options

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# The Gem in a box Gem file
+gem 'geminabox'
--- reactive/geminabox.py
+++ reactive/geminabox.py
@@ -3,15 +3,8 @@ from charms.reactive import when, when_not, set_state
 
 @when_not('geminabox.installed')
 def install_geminabox():
-    # Do your setup here.
     #
-    # If your charm has other dependencies before it can install,
-    # add those as @when() clauses above., or as additional @when()
-    # decorated handlers below
-    #
-    # See the following for information about reactive charms:
-    #
-    #  * https://jujucharms.com/docs/devel/developer-getting-started
-    #  * https://github.com/juju-solutions/layer-basic#overview
-    #
+
+
+
     set_state('geminabox.installed')
